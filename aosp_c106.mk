d4fd770171359ad7da35f1537a79a3c1e3807604
Mustafa Shaikh <smustfa147@gmail.com>
2019-09-05 11:10:07 +0000

    C106: Add updatable_apex.mk

--- aosp_c106.mk
+++ aosp_c106.mk
@@ -23,6 +23,9 @@ $(call inherit-product, device/coolpad/c106/full_c106.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := c106
 PRODUCT_NAME := aosp_c106
