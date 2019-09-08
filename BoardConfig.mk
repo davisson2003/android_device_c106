af025d9e7dfa7a41a8d5b49ed21dbc95736bce08
Akash Mondal <a98mondal@gmail.com>
2019-09-08 09:58:41 +0530

    Use ROOT_EXTRA_SYMLINKS for persist, dsp and firmware * create mount point for dsp and firmware at /vendor/dsp and /vendor/firmware_mnt
    * also update fstab accordingly and add classpath for firmware in kernel commandline
        /persist -> /mnt/vendor/persist
        /dsp -> /vendor/dsp
        /firmware -> /vendor/firmware_mnt
    
    * it's better than creating extra folders on root
    
    Signed-off-by: Akash Mondal <a98mondal@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,7 @@ TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := MSM8952
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlyprintk loop.max_part=7
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci earlyprintk loop.max_part=7 firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
@@ -244,7 +244,10 @@ TARGET_LD_SHIM_LIBS := \
    /system/bin/gx_fpd|fakelogprint.so
 
 # Root Directories
-BOARD_ROOT_EXTRA_FOLDERS := dsp firmware persist
+BOARD_ROOT_EXTRA_SYMLINKS := \
+    /mnt/vendor/persist:/persist \
+    /vendor/dsp:/dsp \
+    /vendor/firmware_mnt:/firmware
 
 # Telephony
 TARGET_USES_ALTERNATIVE_MANUAL_NETWORK_SELECT := true
