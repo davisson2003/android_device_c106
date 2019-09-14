7464124fc47b90bcbdcd573b44dee492cdbb8778
Akash Mondal <a98mondal@gmail.com>
2019-09-15 00:28:22 +0530

    Revert "s2: Drop audio amplifier for now"
    This reverts commit 61064d993214660f43cfd4b28d76247bc79b7405.

--- device.mk
+++ device.mk
@@ -104,6 +104,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     audio.primary.msm8952 \
     audio.a2dp.default \
+    audio_amplifier.msm8952 \
     audio.r_submix.default \
     audio.usb.default
 
