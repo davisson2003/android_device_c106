9bad576ae29a25e53eed7184de87be3188d395c2
Mustafa Shaikh <smustfa147@gmail.com>
2019-10-06 13:36:45 +0000

    C106: add firmware_mnt

--- config.fs
+++ config.fs
@@ -154,6 +154,12 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
 
+[vendor/firmware_mnt/image/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps:0
+
 [bt_firmware/]
 mode: 0771
 user: AID_SYSTEM
